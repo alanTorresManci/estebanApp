5ef159341cda51b629621da5c8c80e71e8ac5ebb
Alan Torres <alantorres@Alans-MacBook-Air.local>
2019-02-28 15:41:24 +0100

    data

--- app.js
+++ app.js
@@ -6,43 +6,19 @@ const path = require('path');
 const app = express();
 
 const middleware = require('./middleware');
-const { getHomePage } = require('./routes/index');
 const { registro, login, guest } = require('./routes/user');
-// const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
+
 const port = 5000;
 const { check, validationResult } = require('express-validator/check')
 
-
-// create connection to database
-// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'estebanDB',
-    port: '8889',
-});
-
-// connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
-});
-global.db = db;
-
 // configure middleware
-app.set('port', process.env.port || port); // set express to use this port
-app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
-app.set('view engine', 'ejs'); // configure template engine
+app.set('port', process.env.port || port);
+app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // parse form data client
-app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
-app.use(fileUpload()); // configure fileupload
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes for the app
-app.get('/', getHomePage);
 app.post('/registro', [
     check('name').not().isEmpty(),
     check('last_name').not().isEmpty(),
